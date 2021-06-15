73cebe81f53eca3eaebc39e33dcf41fc5d43b3db
ajish097 <ajish097@gmail.com>
2021-06-15 13:34:23 +0530

    adding back components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import "./Header.css";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        alt="hacker-news"
+        className="header-child"
+        style={{ border: "1px white solid" }}
+        src="https://news.ycombinator.com/y18.gif"
+      />
+      <h2 className="header-child">
+        <a href="/">Hacker News</a>
+      </h2>
+    </div>
+  );
+};
+
+export default Header;
